c6a81525f25ef00f19016db59a48095a4c1a014d
Eman Mohamed <emanmohamedsr72@gmail.com>
2025-08-06 06:53:12 +0300

    handle delete opened file tab

--- src/components/OpendFileBarTab.tsx
+++ src/components/OpendFileBarTab.tsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import type { IFile } from "../interfaces";
 import FileRenderIcon from "./FileRenderIcon";
 import CloseIcon from "./SVG/CloseIcon";
-import { setClickedFile } from "../app/features/fileTreeSlice";
+import { setClickedFile, setOpenedFiles } from "../app/features/fileTreeSlice";
 import type { RootState } from "../app/store";
 
 interface Iprops {
@@ -10,9 +10,26 @@ interface Iprops {
 }
 
 const OpendFileBarTab = ({ file: { id, name, content } }: Iprops) => {
-	const { clickedFile } = useSelector(({ fileTree }: RootState) => fileTree);
+	const { openedFiles, clickedFile } = useSelector(
+		({ fileTree }: RootState) => fileTree,
+	);
 	const dispatch = useDispatch();
 	//** Handlers */
+	const onRemoveTab = () => {
+		const filteredTabs = openedFiles.filter((file) => file.id !== id);
+		const lastTab = filteredTabs[filteredTabs.length - 1];
+		dispatch(setOpenedFiles(filteredTabs));
+		if (lastTab)
+			dispatch(
+				setClickedFile({
+					fileId: lastTab.id,
+					fileName: lastTab.name,
+					fileContent: lastTab.content || "",
+				}),
+			);
+		else
+			dispatch(setClickedFile({ fileId: "", fileName: "", fileContent: "" }));
+	};
 	const activateFileHandler = () => {
 		dispatch(
 			setClickedFile({
@@ -32,7 +49,12 @@ const OpendFileBarTab = ({ file: { id, name, content } }: Iprops) => {
 				<FileRenderIcon name={name} />
 				<p className='flex-shrink-1'>{name}</p>
 			</div>
-			<div className='p-0.5 hover:bg-white/15'>
+			<div
+				className='p-0.5 hover:bg-white/15 '
+				onClick={(e) => {
+					e.stopPropagation();
+					onRemoveTab();
+				}}>
 				<CloseIcon />
 			</div>
 		</div>
